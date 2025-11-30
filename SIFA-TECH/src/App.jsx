379e65f3e086d53d2e867740dbaa5d636e1c172f
Inayat Fatima Qureshi <inayat.fatima.qureshi@gmail.com>
2025-11-30 14:14:49 +0800

    installed and setup vite and tailwind css

---
+++ SIFA-TECH/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='bg-blue-500 text-white'>Home</div>
+  )
+}
+
+export default App
